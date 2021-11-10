055a9f4f1f32d072f914ccf7d3dcf391d2fe9725
alxkzncoff <alx.kzncoff@gmail.com>
2021-11-10 15:11:51 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008]

---
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -0,0 +1,169 @@
+package ru.job4j.map;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Реализация простого словаря.
+ * @param <K> ключ
+ * @param <V> значение
+ * @author Aleksandr Kuznetsov
+ * @version 1.0
+ */
+public class SimpleMap<K, V> implements Map<K, V> {
+
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private int capacity = 8;
+
+    private int count = 0;
+
+    private int modCount = 0;
+
+    private MapEntry<K, V>[] table = new MapEntry[capacity];
+
+    /**
+     * Метод добавляет пару ключ/значение в словарь. Без решения коллизий.
+     * Если отсутствует место, то пара не добавляется в словарь.
+     * @param key ключ
+     * @param value значение
+     * @return true если пара успешно добавлена, иначе false.
+     */
+    @Override
+    public boolean put(K key, V value) {
+        boolean rsl = false;
+        if ((float) count / capacity >= LOAD_FACTOR) {
+            expand();
+        }
+        int hash = (key == null) ? 0 : hash(key.hashCode());
+        int index = indexFor(hash);
+        MapEntry<K, V> pair = new MapEntry<>(key, value);
+        if (get(key) == null) {
+            table[index] = pair;
+            modCount++;
+            count++;
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод вычисляет хэш ключа.
+     * @param hashcode хэш-код.
+     * @return int.
+     */
+    private int hash(int hashcode) {
+        return (hashcode) ^ (hashcode >>> 16);
+    }
+
+    /**
+     * Метод вычисляет индекс, по которому нужно разместить пару ключ/значение
+     * во внутренней таблице словаря.
+     * @param hash хэш.
+     * @return индекс.
+     */
+    private int indexFor(int hash) {
+        return hash % capacity;
+    }
+
+    /**
+     * Метод увеличивает размер внутренней таблицы словаря в два раза.
+     */
+    private void expand() {
+        capacity *= 2;
+        int hash;
+        int index;
+        MapEntry<K, V>[] temp = new MapEntry[capacity];
+        for (MapEntry<K, V> kvMapEntry : table) {
+            hash = (kvMapEntry.key == null) ? 0 : hash(kvMapEntry.key.hashCode());
+            index = indexFor(hash);
+            temp[index] = kvMapEntry;
+        }
+        table = temp;
+    }
+
+    /**
+     * Метод возвращает значение, которое соответствует указанному ключу, иначе
+     * возвращает null.
+     * @param key ключ
+     * @return значение соответствующее ключу или null.
+     */
+    @Override
+    public V get(K key) {
+        V value = null;
+        int hash = (key == null) ? 0 : hash(key.hashCode());
+        int index = indexFor(hash);
+        if (table[index] != null) {
+            value = table[index].value;
+        }
+        return value;
+    }
+
+    /**
+     * Метод удаляет пару ключ/значение.
+     * @param key ключ.
+     * @return true, если пара успешно удалена, иначе false.
+     */
+    @Override
+    public boolean remove(K key) {
+        boolean rsl = false;
+        if (get(key) != null) {
+            int hash = (key == null) ? 0 : hash(key.hashCode());
+            int index = indexFor(hash);
+            table[index] = null;
+            count--;
+            modCount++;
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return new Iterator<>() {
+            private final int expectedModCount = modCount;
+
+            private int point = 0;
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return point < count;
+            }
+
+            @Override
+            public K next() {
+                K rsl = null;
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                for (; index < table.length; index++) {
+                    if (table[index] != null) {
+                        rsl = table[index].key;
+                        point++;
+                        index++;
+                        break;
+                    }
+                }
+                return rsl;
+            }
+
+        };
+    }
+
+    private static class MapEntry<K, V> {
+
+        private final K key;
+        private final V value;
+
+        public MapEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+    }
+}
---
+++ src/test/java/ru/job4j/map/SimpleMapTest.java
@@ -0,0 +1,72 @@
+package ru.job4j.map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+
+public class SimpleMapTest {
+
+    @Test
+    public void putTrue() {
+       SimpleMap<String, Integer> map = new SimpleMap<>();
+       Assert.assertTrue(map.put("a", 1));
+    }
+
+    @Test
+    public void putFalse() {
+        SimpleMap<String, Integer> map = new SimpleMap<>();
+        map.put("a", 1);
+        Assert.assertFalse(map.put("a", 2));
+    }
+
+    @Test
+    public void get() {
+        SimpleMap<String, Integer> map = new SimpleMap<>();
+        map.put("a", 1);
+        Assert.assertThat(map.get("a"), is(1));
+    }
+
+    @Test
+    public void getNull() {
+        SimpleMap<String, Integer> map = new SimpleMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+        Assert.assertThat(map.get("c"), is(nullValue()));
+    }
+
+    @Test
+    public void removeTrue() {
+        SimpleMap<String, Integer> map = new SimpleMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+        Assert.assertTrue(map.remove("a"));
+    }
+
+    @Test
+    public void removeFalse() {
+        SimpleMap<String, Integer> map = new SimpleMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+        Assert.assertFalse(map.remove("c"));
+    }
+
+    @Test
+    public void iteration() {
+        SimpleMap<String, Integer> map = new SimpleMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+        map.put("c", 3);
+        Iterator<String> iterator = map.iterator();
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertThat(iterator.next(), is("a"));
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertThat(iterator.next(), is("b"));
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertThat(iterator.next(), is("c"));
+        Assert.assertFalse(iterator.hasNext());
+    }
+}
