8de16f9a080f0ac9158e76eb5025b0270afaffcf
alxkzncoff <alx.kzncoff@gmail.com>
2021-11-23 16:04:02 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,50 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
+/**
+ * Класс анализирует статус сервера и выводит период времени,
+ * когда сервер был не доступен.
+ * @author Aleksandr Kuzntesov.
+ * @version 1.0
+ */
+public class Analizy {
+    public void unavailable(String source, String target) {
+        List<String> in = new ArrayList<>();
+        boolean serverUp = true;
+        String period = "";
+        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
+            read.lines().forEach(in::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (String line: in) {
+            if ((line.startsWith("4") || line.startsWith("5")) && serverUp) {
+                period = period.concat(line.split(" ")[1] + ";");
+                serverUp = false;
+            }
+            if ((line.startsWith("2") || line.startsWith("3")) && !serverUp) {
+                period = period.concat(line.split(" ")[1] + ";" + System.lineSeparator());
+                serverUp = true;
+                try (PrintWriter out = new PrintWriter(
+                        new BufferedOutputStream(
+                                new FileOutputStream(target)
+                        ))) {
+                    out.println(period);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String source = "./data/server.log";
+        String target = "./data/unavailable.csv";
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source, target);
+    }
+}
