239f1003966b56aa39a1b45f50ddb673357adc3f
alxkzncoff <alx.kzncoff@gmail.com>
2021-12-22 17:02:23 +0300

    1. Представления [#504792]

--- sql/cars/script.sql
+++ sql/cars/script.sql
@@ -22,3 +22,18 @@ where c.name is null;
 select t.name from car c
 right join transmission t on  t.id = c.transmission_id
 where c.name is null;
+
+-- Запрос с помощью представления
+create view petrol_with_two_doors as
+	select c.name as Model, 
+			b.type as Body, 
+			e.name as Engine, 
+			e.power as Power, 
+			t.name as Transmission 
+		from car c
+		left join body b on b.id = c.body_id
+		left join engine e on e.id = c.engine_id
+		left join transmission t on t.id = c.transmission_id
+		where b.type = '2 doors' and e.name = 'petrol';
+		
+select * from petrol_with_two_doors;
